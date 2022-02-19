80835d6ba9174458412888d5964cc35d945a00e4
kumar147shubham <kumar147shubham@gmail.com>
2022-02-19 19:02:24 +0530

    Event,Command,Message Interface and class created

---
+++ MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime timestamp { get; protected set; }
+        protected Command()
+        {
+            timestamp = DateTime.Now;
+        }
+    }
+}
