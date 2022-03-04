1ed6b9b610db47ff717a5338f3a928bce5ac8a3d
kumar147shubham <kumar147shubham@gmail.com>
2022-03-04 23:41:37 +0530

    Implement Publish method and DI Container

---
+++ MicroRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
