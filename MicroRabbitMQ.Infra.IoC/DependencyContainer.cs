f2f8ac90c10fd2a67897864ab113eeb980c4639d
kumar147shubham <kumar147shubham@gmail.com>
2022-03-09 09:41:13 +0530

    create even,eventHandler for Tranfer and register in DI

--- MicroRabbitMQ.Infra.IoC/DependencyContainer.cs
+++ MicroRabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -12,6 +12,8 @@ using MicroRabbit.Transfer.Application.Interfaces;
 using MicroRabbit.Transfer.Application.Services;
 using MicroRabbit.Transfer.Data.Context;
 using MicroRabbit.Transfer.Data.Repository;
+using MicroRabbit.Transfer.Domain.EventHandlers;
+using MicroRabbit.Transfer.Domain.Events;
 using MicroRabbit.Transfer.Domain.Interface;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -28,6 +30,9 @@ namespace MicroRabbitMQ.Infra.IoC
         {
             //Domain Bus
             services.AddTransient<IEventBus, RabbitMQBus>();
+
+            //Domain Events
+            services.AddTransient<IEventHandler<TransferCreatedEvent>, TransferEventHandler>();
 
             // Domain Banking Commands
             services.AddTransient<IRequestHandler<CreateTransferCommand, bool>, TransferCommandHandler>();
